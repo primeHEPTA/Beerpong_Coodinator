ac48c1da076e73212c7b8133ba0843e78ec4df5d
primeHEPTA <131783323+primeHEPTA@users.noreply.github.com>
2023-11-03 17:13:00 +0100

    Update game_phase.py

--- game_phase.py
+++ game_phase.py
@@ -1,248 +1,105 @@
-import random
-
-import numpy as np
-
-from setup_phase import create_gameplan, create_groupstage, draw_teams, names, num_players,mode
-
-
-
-class Tournament:
-
-    def __init__(self, group_names):
-
-        self.group_names = group_names
-
-        self.groups = {group: [] for group in group_names}
-
-        self.results = {group: {} for group in group_names}
-
-        self.standings = {group: {} for group in group_names}
-
-
-
-    
-
-    def record_result(self, group, playday, match, becher_diff):
-
-        if group not in self.results:
-
-            print(f"Gruppe '{group}' existiert nicht.")
-
-            return
-
-        
-
-        if playday not in self.results[group]:
-
-            self.results[group][playday] = []
-
-        
-
-        self.results[group][playday].append((match, becher_diff))
-
-
-
-    def calculate_standings(self, group):
-
-        if group not in self.groups:
-
-            print(f"Gruppe '{group}' existiert nicht.")
-
-            return
-
-        
-
-        # Tabellenstände berechnen
-
-        standings = {team: [0, 0, 0, 0, 0] for team in self.groups[group]}
-
-        # [Spiele, Siege, Niederlagen, Becherdifferenz, Punkte]
-
-        
-
-        for playday, results in self.results[group].items():
-
-            for match, becher_diff in results:
-
-                team1, team2 = match
-
-                if becher_diff > 0:
-
-                    standings[team1][1] += 1  # Sieg für Team 1
-
-                    standings[team2][2] += 1  # Niederlage für Team 2
-
-                    standings[team1][4] += 3  # Punkte für Team 1
-
-                elif becher_diff < 0:
-
-                    standings[team1][2] += 1  # Niederlage für Team 1
-
-                    standings[team2][1] += 1  # Sieg für Team 2
-
-                    standings[team2][4] += 3  # Punkte für Team 2
-
-                # Becherdifferenz hinzufügen
-
-                standings[team1][3] += becher_diff
-
-                standings[team2][3] -= becher_diff
-
-                # Gespielte Spiele erhöhen
-
-                standings[team1][0] += 1
-
-                standings[team2][0] += 1
-
-        
-
-        # Nach Punkten sortieren
-
-        sorted_standings = sorted(standings.items(), key=lambda x: (x[1][4], x[1][3]), reverse=True)
-
-        self.standings[group] = sorted_standings
-
-
-
-    def display_standings(self, group):
-
-        if group not in self.groups:
-
-            print(f"Gruppe '{group}' existiert nicht.")
-
-            return
-
-        
-
-        if group not in self.standings:
-
-            print(f"Tabellenstände für Gruppe '{group}' sind noch nicht berechnet.")
-
-            return
-
-        
-
-        print(f"Tabellenstände für Gruppe '{group}':")
-
-        print("Team\tSpiele\tSiege\tNiederlagen\tBecherdiff\tPunkte")
-
-        for team, stats in self.standings[group]:
-
-            print(f"{team}\t{stats[0]}\t{stats[1]}\t{stats[2]}\t{stats[3]}\t{stats[4]}")
-
+from setup_phase import create_gameplan, create_groupstage, draw_teams, names, num_players, mode
+#from tournament_class import Tournament
+from get_groupname import Tournament
+def get_group_name(teams, group_names):    
+    for group in range(len(groups)):
+        if all(team in groups[group] for team in teams):
+            return group_names[group]
+    return None 
    
-
-
-
-def game_phase(tournament, gameplan):
-
+def game_phase(tournament, group_names, playdaymatches_list):
     while True:
-
-        playday = input("Welcher Spieltag ('0' zum Beenden): ")
-
-        if playday == '0':
-
+        command = input("Enter Command ('s' for standings, 'e' for enter scoring, '0' for termination): ")
+        if command == '0':
             break
+        if command == 's':
+            group_name = input("Enter Groupname ('Group(number)'): ")
+            if group_name in tournament.group_names:
+                tournament.calculate_standings(group_name)
+                tournament.display_standings(group_name)
+            else:
+                print(f"Group '{group_name}' does not exist! Please note case sensitivity!")
+        elif command == 'e':
+            playday = input("Which Matchday ('0' for termination): ")
+            if playday == '0':
+                break
 
+            try:
+                playday = int(playday)
+            except ValueError:
+                print("Invalid Matchday! Please enter a valid number")
+                continue
 
+            if playday <= 0 or playday > len(playdaymatches_list):
+                print(f"There is no Matchday {playday}.")
+                continue
 
-        found_playday = False
+            while True:
+                try:
+                    for i, match in enumerate(playdaymatches_list[playday - 1], 1):
+                        print(f"{i}: {match[0]} vs {match[1]}")
 
-        for playday_matches in gameplan:
+                    choice = input("Enter the number of the match to update ('0' to exit): ")
 
-            if playday_matches.startswith(f"Spieltag {playday}"):
+                    if choice == '0':
+                        break
 
-                found_playday = True
-
-                matches = playday_matches[len(f"Spieltag {playday}: "):]
-
-                matches = matches.split(", ")
-
-                for match in matches:
-
-                    teams = match.split(" vs ")
-
-                    if len(teams) == 2:
-
-                        team1, team2 = teams
-
-                        becher_diff = int(input(f"Becherdifferenz im Spiel {team1} vs {team2}: "))
-
-                        tournament.record_result("Group1", int(playday), (team1, team2), becher_diff)
-
+                    match_number = int(choice)
+                    if 1 <= match_number <= len(playdaymatches_list[playday - 1]):
+                        team1, team2 = playdaymatches_list[playday - 1][match_number - 1]
+                        print(f"Match: {team1} vs {team2}")
+                        group_name = get_group_name([team1, team2], group_names)
+                        if group_name:
+                            cup_diff = int(input(f"Differenece for Match {team1} vs {team2}: "))                           
+                            tournament.record_result(group_name, playday, (team1, team2), cup_diff)
+                        else:
+                            print("Group not found for the selected teams.")
                     else:
+                        print("Invalid match number. Choose a valid match or '0' to exit.")
 
-                        print(f"Ungültiges Spielformat: {match}")
-
-
-
-        if not found_playday:
-
-            print(f"Es gibt keine Spiele für Spieltag {playday}.")
+                except ValueError:
+                    print("Invalid Input! Cup difference should be a number.")
+        else:
+            print("Invalid command! Use 's' for standings, 'e' for enter scoring, '0' for termination ")
 
 
-
-        for group_name in group_names:
-
-            tournament.calculate_standings(group_name)
-
-            tournament.display_standings(group_name)       
-
-
-
+####################################
 #MAIN
-
-
-
+####################################
 
 
 mode_select = mode()
-
 num_of_players = num_players(mode_select)
-
 player_names=names(num_of_players)
-
-
 
 if mode_select==1:              
 
     teams=draw_teams(player_names)  #Draw Teams for Duo CashCup if Duo mode was selected
-
     groups,group_names=create_groupstage(teams)
-
     for i, group in enumerate(groups):
 
         print(f"Group {i + 1}: {group}")
 
-    gameplan = create_gameplan(groups)
-
+    gameplan,playdaymatches_list = create_gameplan(groups)
     for playday_matches in gameplan:
-
         print(playday_matches)
-
-    tournament = Tournament(group_names)
-
-    game_phase(tournament,gameplan)
+        
+    tournament = Tournament(group_names,player_names, playdaymatches_list,groups)
+    game_phase(tournament, group_names,playdaymatches_list)
 
 else:
-
     groups,group_names=create_groupstage(player_names)
-
-  
-
+    print(group_names)
     for i, group in enumerate(groups):
-
         print(f"Group {i + 1}: {group}")
-
-    gameplan = create_gameplan(groups)
-
+        
+    gameplan,playdaymatches_list = create_gameplan(groups)
+    #print(playdaymatches_list[0])
     for playday_matches in gameplan:
-
         print(playday_matches)
-
-    tournament = Tournament(group_names)
-
-    game_phase(tournament,gameplan)
+        
+    tournament = Tournament(group_names, player_names, playdaymatches_list, groups)
+    game_phase(tournament, group_names, playdaymatches_list)
 
     
 
